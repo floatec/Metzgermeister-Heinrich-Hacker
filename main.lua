119f41723072028a0e1fae564be0fe6561f020a2
floatec <mp@floatec.de>
2013-05-31 13:57:06 +0200

    doping

--- main.lua
+++ main.lua
@@ -14,6 +14,7 @@ function love.load()
 	-- load images
 	images = {
 		heinrich = love.graphics.newImage("gfx/bucher.png"),
+		child = love.graphics.newImage("gfx/child.png"),
 	}
 	
 	-- sound effect
@@ -30,10 +31,13 @@ function love.load()
 	height = love.graphics.getHeight()
 	
 	-- player
-	x, y = 32, 32
+	x, y = 31, 31
 	w = images.heinrich:getWidth()
 	h = images.heinrich:getHeight()
 	speed = 300	
+
+	--child
+	spawn_Child()
 end
 
 function love.update(dt)
@@ -51,8 +55,31 @@ function love.update(dt)
 	elseif love.keyboard.isDown("down") then
 		newy = y + speed * dt
 	end
-	if(can_move_to(newx,newy,32,32)) then
-		x,y=newx,newy
+	if(can_move_to(x,newy,32,32)) then
+		x,y=x,newy
+	end
+	if(can_move_to(newx,y,32,32)) then
+		x,y=newx,y
+	end
+
+	-- update childs
+	for i, v in ipairs(childs) do
+		print (v.direction)
+		newy=v.y
+		newx=v.x
+		if v.direction%2==0 then
+			newx = v.x - speed*0.3 * (v.direction-3) * dt
+		else 	
+			newy = v.y - speed*0.3 * (v.direction-2) * dt
+		end
+		-- wall hit
+		if can_move_to(newx,newy,32,32) then
+			v.x,v.y=newx,newy
+		else
+			print("change direction")
+			v.direction = math.random(1,4)
+		end
+		print (v.x.." "..v.y)
 	end
 end
 
@@ -65,6 +92,10 @@ function love.draw()
 	-- Draws the map
 	map:draw()
 
+	--childs
+	for i, v in ipairs(childs) do
+		love.graphics.draw(images.child, v.x, v.y, 0 , v.scale, v.scale)
+	end
 		
 	-- player
 	love.graphics.draw(images.heinrich, x, y)
@@ -90,4 +121,19 @@ function is_colliding_with_wall(x,y)
 	
 	return false
 end
+childs={}
+function spawn_Child()
 
+	local t = {}
+	
+	-- size
+	t.scale = 1
+	t.w = images.child:getWidth() * t.scale
+	t.h = images.child:getHeight() * t.scale
+	t.direction = math.random(1,4) 
+	-- position
+	t.x = 31
+	t.y = 31
+	
+	table.insert(childs, t)
+end
