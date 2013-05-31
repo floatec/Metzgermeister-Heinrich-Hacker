7b452e6264d3437f718831d81f56ac8acd921a15
M4riuZ <code@mariuz.org>
2013-05-31 15:27:22 +0200

    Refactored into multiple files

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 require("AnAL")
+require("global")
+require("collision")
 loader = require("AdvTiledLoader/loader")
-
- 
 
 function love.load()
 	-- Path to the tmx files. The file structure must be similar to how they are saved in Tiled
@@ -31,55 +31,42 @@ function love.load()
 	height = love.graphics.getHeight()
 	
 	-- player
-	x, y = 31, 31
-	w = images.heinrich:getWidth()
-	h = images.heinrich:getHeight()
-	speed = 300	
+	player = {
+		x = 31, 
+		y = 31,
+		w = images.heinrich:getWidth(),
+		h = images.heinrich:getHeight()
+	}
 
 	--child
 	spawn_Child()
 end
 
 function love.update(dt)
-	newx,newy=x,y
+	newx, newy = p.x, p.y
 	-- update x
 	if love.keyboard.isDown("left")   then
-		newx = x - speed * dt
+		newx = p.x - speed * dt
 	elseif love.keyboard.isDown("right") then
-		newx = x + speed * dt
+		newx = p.x + speed * dt
 	end
 
 	-- update y
 	if love.keyboard.isDown("up")  then
-		newy = y - speed * dt
+		newy = p.y - speed * dt
 	elseif love.keyboard.isDown("down") then
-		newy = y + speed * dt
+		newy = p.y + speed * dt
 	end
-	if(can_move_to(x,newy,32,32)) then
-		x,y=x,newy
+	if(can_move_to(p.x, newy, 32, 32)) then
+		p.x, p.y = p.x, newy
 	end
-	if(can_move_to(newx,y,32,32)) then
-		x,y=newx,y
+	if(can_move_to(newx, p.y, 32, 32)) then
+		p.x, p.y = newx, p.y
 	end
 
 	-- update childs
 	for i, v in ipairs(childs) do
-		print (v.direction)
-		newy=v.y
-		newx=v.x
-		if v.direction%2==0 then
-			newx = v.x - speed*0.3 * (v.direction-3) * dt
-		else 	
-			newy = v.y - speed*0.3 * (v.direction-2) * dt
-		end
-		-- wall hit
-		if can_move_to(newx,newy,32,32) then
-			v.x,v.y=newx,newy
-		else
-			print("change direction")
-			v.direction = math.random(1,4)
-		end
-		print (v.x.." "..v.y)
+		move(v, dt)
 	end
 end
 
@@ -98,29 +85,13 @@ function love.draw()
 	end
 		
 	-- player
-	love.graphics.draw(images.heinrich, x, y)
+	love.graphics.draw(images.heinrich, p.x, p.y)
 	
 	-- text
 	love.graphics.setColor(255, 0, 0)
     love.graphics.printf("Metzgermeister Heinrich Hacker", 0, 10, width, "center")	
 end
 
-
-function can_move_to( x,y ,w,h)
-	 if is_colliding_with_wall(x,y) or  is_colliding_with_wall(x+w,y) or  is_colliding_with_wall(x,y+h) or  is_colliding_with_wall(x+w,y+h) then
-	 	return false
-	 end
-	 	return true
-end
-
-function is_colliding_with_wall(x,y)
-	local tile=map("buildings")(math.floor(x/16),math.floor(y/16))
-	if tile and tile.properties["wall"] then
-		return true
-	end
-	
-	return false
-end
 childs={}
 function spawn_Child()
 
